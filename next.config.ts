4251147350be1716571d868be010357ea9717257
neo_yeh <neo_yeh@htc.com>
2025-02-03 11:47:33 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
   distDir: 'dist',
+  basePath: '/new-portfolio',
+  assetPrefix: '/new-portfolio/',
 };
 
 export default nextConfig;
